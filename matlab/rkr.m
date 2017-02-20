3700d547a2d2e14a988904ef1ddef2ebc51d2143
boonleng <boonleng@ou.edu>
2017-02-20 15:44:52 -0600

    Viewing 11 hops (22 columns)

--- matlab/rkr.m
+++ matlab/rkr.m
@@ -19,14 +19,15 @@ pulses = single(pulses);
 
 %%
 N = 6;
-ns = 400;
-t = (1:ns) / 100;
+ng = min(size(pulses, 1), 90);
+ns = min(size(pulses, 2), 500);
 
-ng = min(size(pulses, 2), 500);
+t_fast = (1:ng) / 50;
+t_slow = (1:ns) / 100;
 
 figure(1)
 clf
-imagesc(20 * log10(abs(pulses(1:ns, 1:ng, 2))))
+imagesc(20 * log10(abs(pulses(1:ng, 1:ns, 2))))
 set(gca, 'YDir', 'Normal')
 colorbar
 boonlib('bsizewin', gcf, [800 400])
@@ -35,7 +36,7 @@ xlim([0 100])
 %%
 figure(2)
 clf
-M = 8;
+M = 22;
 ha = zeros(M * N, 1);
 hd = zeros(M * N, 3);
 ht = zeros(M * N, 1);
@@ -43,17 +44,18 @@ for ii = 1 : M * N
     k = ii;
     ix = rem(ii - 1, M);
     iy = N - 1 - floor((ii - 1) / M);
-    ha(ii) = axes('Unit', 'Normalized', 'Position', [0.04 + ix / M * 0.94, 0.05 + iy / N * 0.94, 0.88 / M, 0.88 / N]);
-    hd(ii, :) = plot(t, real(pulses(1:ns, k, 2)), ...
-        t, imag(pulses(1:ns, k, 2)), ...
-        t, abs(pulses(1:ns, k, 2)));
+    ha(ii) = axes('Unit', 'Normalized', 'Position', [0.02 + ix / M * 0.97, 0.05 + iy / N * 0.94, 0.92 / M, 0.88 / N]);
+    hd(ii, :) = plot(...
+        t_fast, real(pulses(1:ng, k, 2)), ...
+        t_fast, imag(pulses(1:ng, k, 2)), ...
+        t_fast, abs(pulses(1:ng, k, 2)));
     grid on
     if iy == 0, xlabel('Time (us)'); else, set(gca, 'XTickLabel', []); end
     if ix == 0, ylabel('Amplitude'); else, set(gca, 'YTickLabel', []); end
-    ht(ii) = text(0.9 * t(end), -5000, sprintf('%d %d / %d', dat.pulses(k).n, dat.pulses(k).i, rem(dat.pulses(k).i, 8)));
-    ylim(5900 * [-1 1])
+    ht(ii) = text(0.9 * t_fast(end), -10000, sprintf('%d %d / %d', dat.pulses(k).n, dat.pulses(k).i, rem(dat.pulses(k).i, 8)));
+    ylim(11500 * [-1 1])
 end
-boonlib('bsizewin', gcf, [1920 1080])
+boonlib('bsizewin', gcf, [3200 1080])
 set(ht, 'HorizontalAlignment', 'Right');
 
 return
