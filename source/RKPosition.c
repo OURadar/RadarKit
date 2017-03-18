6170f4ca937387d8e4db9d1b0e9acd3f61f57a98
boonleng <boonleng@ou.edu>
2017-03-18 09:46:49 -0400

    Pedestal flag interpretation

--- source/RKPosition.c
+++ source/RKPosition.c
@@ -19,11 +19,11 @@ void RKPositionnUpdateStatusString(RKPositionEngine *engine);
 
 #define RKPositionAzimuthFlagColor(x)                           \
 (x & RKPositionFlagAzimuthError ? "\033[91m" :                  \
-(x & RKPositionFlagAzimuthEnabled ? "\033[92m" : "\033[91m"))
+(x & RKPositionFlagAzimuthEnabled ? "\033[92m" : "\033[93m"))
 
 #define RKPositionElevationFlagColor(x)                         \
 (x & RKPositionFlagElevationError ? "\033[91m" :                \
-(x & RKPositionFlagElevationEnabled ? "\033[92m" : "\033[91m"))
+(x & RKPositionFlagElevationEnabled ? "\033[92m" : "\033[93m"))
 
 void RKPositionnUpdateStatusString(RKPositionEngine *engine) {
     int i;
@@ -60,7 +60,7 @@ void RKPositionnUpdateStatusString(RKPositionEngine *engine) {
     string[i] = '#';
     i = RKStatusBarWidth + sprintf(string + RKStatusBarWidth, " %04d |", *engine->positionIndex);
     RKPosition *position = &engine->positionBuffer[RKPreviousModuloS(*engine->positionIndex, engine->positionBufferDepth)];
-    snprintf(string + i,RKMaximumStringLength - i, " %010llu  %sAZ%s %6.2f° @ %+7.2f°/s [%6.2f°]   %sEL%s %6.2f° @ %+6.2f°/s [%6.2f°]",
+    snprintf(string + i,RKMaximumStringLength - i, " %010llu  %sAZ%s %6.2f° @ %+7.2f°/s [%6.2f°]   %sEL%s %6.2f° @ %+6.2f°/s [%6.2f°]  %08x",
              (unsigned long long)position->i,
              rkGlobalParameters.showColor ? RKPositionAzimuthFlagColor(position->flag) : "",
              rkGlobalParameters.showColor ? RKNoColor : "",
@@ -71,7 +71,8 @@ void RKPositionnUpdateStatusString(RKPositionEngine *engine) {
              rkGlobalParameters.showColor ? RKNoColor : "",
              position->elevationDegrees,
              position->elevationVelocityDegreesPerSecond,
-             position->sweepElevationDegrees);
+             position->sweepElevationDegrees,
+             position->flag);
 
     engine->statusBufferIndex = RKNextModuloS(engine->statusBufferIndex, RKBufferSSlotCount);
 }
