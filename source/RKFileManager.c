0808eff855403c4cc00320a7c66b8be1604c4951
boonleng <boonleng@ou.edu>
2017-04-02 11:42:24 -0500

    File list sorting by birth time

--- source/RKFileManager.c
+++ source/RKFileManager.c
@@ -8,7 +8,22 @@
 
 #include <RadarKit/RKFileManager.h>
 
+#define RKFileManagerCapacity  10000
+
+typedef char RKFilename[RKNameLength];
+typedef struct _rk_indexed_stat {
+    int      index;
+    time_t   time;
+    size_t   size;
+} RKIndexedStat;
+
 #pragma mark - Helper Functions
+
+static int struct_cmp_by_time(const void *a, const void *b) {
+    RKIndexedStat *x = (RKIndexedStat *)a;
+    RKIndexedStat *y = (RKIndexedStat *)b;
+    return (int)(x->time - y->time);
+}
 
 #pragma mark - Delegate Workers
 
@@ -17,7 +32,7 @@ static void *fileWatcher(void *in) {
     
     int k = 0;
     
-    struct stat st;
+    //struct stat st;
     struct dirent *dir;
     char path[RKMaximumPathLength];
     path[RKMaximumPathLength - 1] = '\0';
@@ -43,19 +58,44 @@ static void *fileWatcher(void *in) {
         return (void *)1;
     }
     
+    RKFilename *filenames = (RKFilename *)malloc(RKFileManagerCapacity * sizeof(RKFilename));
+    if (filenames == NULL) {
+        RKLog("%s Error. unable to allocate space for filenames.\n", engine->name);
+    }
+    memset(filenames, 0, RKFileManagerCapacity * sizeof(RKFilename));
+    RKIndexedStat *indexedStats = (RKIndexedStat *)malloc(RKFileManagerCapacity * sizeof(RKIndexedStat));
+    if (indexedStats == NULL) {
+        RKLog("%s Error. unable to allocate space for indexed stats.\n", engine->name);
+    }
+    memset(indexedStats, 0, RKFileManagerCapacity * sizeof(RKIndexedStat));
+    
     // List all the files. Keep a copy of the list
+    k = 0;
+    struct stat fileStat;
+    size_t totalUsage = 0;
     while ((dir = readdir(did)) != NULL) {
         if (dir->d_type == DT_REG) {
-            stat(dir->d_name, &st);
-            printf("%s %lld\n", dir->d_name, st.st_size);
+            sprintf(filenames[k], "%s/%s", path, dir->d_name);
+            stat(filenames[k], &fileStat);
+            // Get creation time
+            indexedStats[k].index = k;
+            indexedStats[k].time = fileStat.st_birthtimespec.tv_sec;
+            indexedStats[k].size = fileStat.st_size;
+            totalUsage += fileStat.st_size;
+            printf("%s %12s B  %ld\n", filenames[k], RKIntegerToCommaStyleString(indexedStats[k].size), indexedStats[k].time);
+            k++;
         }
     }
-
+    printf("Total usage: %s B\n", RKIntegerToCommaStyleString(totalUsage));
+    
+    qsort(indexedStats, k, sizeof(RKIndexedStat), struct_cmp_by_time);
+    
+    while (k > 0) {
+        k--;
+        printf("%d %zu -> %s\n", indexedStats[k].index, indexedStats[k].time, filenames[indexedStats[k].index]);
+    }
+    
     while (engine->state & RKEngineStateActive) {
-//        if ((dir = readdir(did)) != NULL) {
-//            stat(dir->d_name, &st);
-//            printf("%s %d %lld\n", dir->d_name, dir->d_type, st.st_size);
-//        }
         k = 0;
         while (k++ < 10 && engine->state & RKEngineStateActive) {
             usleep(100000);
