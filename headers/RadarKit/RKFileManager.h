a356ac6f5c4026394c1e687b25e627cb3ffb2e06
boonleng <boonleng@ou.edu>
2017-04-02 20:00:17 -0500

    New folder structure of health; multi-threaded file manager

--- headers/RadarKit/RKFileManager.h
+++ headers/RadarKit/RKFileManager.h
@@ -2,6 +2,38 @@
 //  RKFileManager.h
 //  RadarKit
 //
+//  Remove old files so that disk usage does not exceed a user-set limit.
+//  Expected file structure from RadarKit:
+//
+//  .../rootDataFolder/iq/20170401/RK-20170401-012345-E1.0.rkr
+//                                 RK-20170401-012346-E2.0.rkr
+//                                 RK-20170401-012347-E3.0.rkr
+//                                             :
+//  .../rootDataFolder/iq/20170402/RK-20170402-012345-E1.0.rkr
+//                                 RK-20170402-012346-E2.0.rkr
+//                                 RK-20170402-012347-E3.0.rkr
+//                                             :
+//
+//  .../rootDataFolder/moments/20170401/RK-20170401-012345-E1.0-Z.nc
+//                                      RK-20170401-012345-E1.0-V.nc
+//                                      RK-20170401-012345-E1.0-W.nc
+//                                                  :
+//  .../rootDataFolder/moments/20170402/RK-20170402-012347-E3.0-Z.nc
+//                                      RK-20170402-012347-E3.0-V.nc
+//                                      RK-20170402-012347-E3.0-W.nc
+//                                                  :
+//
+//  .../rootDataFolder/health/20170401/RK-20170402-2250.txt
+//                                     RK-20170402-2251.txt
+//                                     RK-20170402-2252.txt
+//                                             :
+//  .../rootDataFolder/health/20170402/RK-20170402-2250.txt
+//                                     RK-20170402-2251.txt
+//                                     RK-20170402-2252.txt
+//                                             :
+//
+//  iq : moment : health ~ 3,000 : 100 : 1
+//
 //  Created by Boon Leng Cheong on 3/11/17.
 //  Copyright © 2017 Boon Leng Cheong. All rights reserved.
 //
@@ -13,7 +45,18 @@
 
 #define RKFileManagerDefaultUsageLimit   (size_t)256 * 1024 * 1024 * 1024
 
-typedef struct rk_file_manager {
+typedef struct rk_file_remover RKFileRemover;
+typedef struct rk_file_manager RKFileManager;
+
+struct rk_file_remover {
+    int                    id;
+    pthread_t              tid;
+    char                   path[RKMaximumPathLength];
+    size_t                 usageLimit;
+    RKFileManager          *parent;
+};
+
+struct rk_file_manager {
     // User set variables
     char                   name[RKNameLength];
     RKRadarDesc            *radarDescription;
@@ -22,12 +65,15 @@ typedef struct rk_file_manager {
     size_t                 rawDataUsagelimit;
     
     // Program set variables
+    int                    workerCount;
+    RKFileRemover          *workers;
     pthread_t              tidFileWatcher;
+    pthread_mutex_t        mutex;
 
     // Status / health
     RKEngineState          state;
     uint32_t               memoryUsage;
-} RKFileManager;
+};
 
 RKFileManager *RKFileManagerInit(void);
 void RKFileManagerFree(RKFileManager *);
--- source/RKFileManager.c
+++ source/RKFileManager.c
@@ -27,47 +27,59 @@ static int struct_cmp_by_time(const void *a, const void *b) {
 
 #pragma mark - Delegate Workers
 
-static void *fileWatcher(void *in) {
-    RKFileManager *engine = (RKFileManager *)in;
+static void *fileRemover(void *in) {
+    RKFileRemover *me = (RKFileRemover *)in;
+    RKFileManager *engine = me->parent;
     
-    int k = 0;
-    
-    //struct stat st;
+    int k;
     struct dirent *dir;
-    char path[RKMaximumPathLength];
-    path[RKMaximumPathLength - 1] = '\0';
     
-    RKLog("%s Started.   mem = %s B  state = %x\n", engine->name, RKIntegerToCommaStyleString(engine->memoryUsage), engine->state);
-    
-    engine->state |= RKEngineStateActive;
-    engine->state ^= RKEngineStateActivating;
-    
-    // Raw data path
-    if (engine->radarDescription != NULL && strlen(engine->radarDescription->dataPath)) {
-        snprintf(path, RKMaximumPathLength - 1, "%s/iq", engine->radarDescription->dataPath);
-    } else if (strlen(engine->dataPath)) {
-        snprintf(path, RKMaximumPathLength - 1, "%s/iq", engine->dataPath);
+    const int c = me->id;
+
+    char name[RKNameLength];
+    if (rkGlobalParameters.showColor) {
+        pthread_mutex_lock(&engine->mutex);
+        k = snprintf(name, RKNameLength - 1, "%s", rkGlobalParameters.showColor ? RKGetColor() : "");
+        pthread_mutex_unlock(&engine->mutex);
     } else {
-        snprintf(path, RKMaximumPathLength - 1, "iq");
+        k = 0;
+    }
+    if (engine->workerCount > 9) {
+        k += sprintf(name + k, "F%02d", c);
+    } else {
+        k += sprintf(name + k, "F%d", c);
+    }
+    if (rkGlobalParameters.showColor) {
+        sprintf(name + k, RKNoColor);
     }
     
-    RKLog("%s Monitoring path '%s'\n", engine->name, path);
-    DIR *did = opendir(path);
-    if (did == NULL) {
-        RKLog("%s Error. Unable to access director '%s'\n", engine->name, path);
-        return (void *)1;
-    }
-    
+    size_t mem = 0;
     RKFilename *filenames = (RKFilename *)malloc(RKFileManagerCapacity * sizeof(RKFilename));
     if (filenames == NULL) {
-        RKLog("%s Error. unable to allocate space for filenames.\n", engine->name);
+        RKLog("%s %s Error. unable to allocate space for filenames.\n", engine->name, name);
     }
     memset(filenames, 0, RKFileManagerCapacity * sizeof(RKFilename));
+    mem += RKFileManagerCapacity * sizeof(RKFilename);
     RKIndexedStat *indexedStats = (RKIndexedStat *)malloc(RKFileManagerCapacity * sizeof(RKIndexedStat));
     if (indexedStats == NULL) {
-        RKLog("%s Error. unable to allocate space for indexed stats.\n", engine->name);
+        RKLog("%s %s Error. unable to allocate space for indexed stats.\n", engine->name, name);
     }
     memset(indexedStats, 0, RKFileManagerCapacity * sizeof(RKIndexedStat));
+    mem += RKFileManagerCapacity * sizeof(RKIndexedStat);
+    
+    pthread_mutex_lock(&engine->mutex);
+    engine->memoryUsage += mem;
+    
+    RKLog(">%s %s Started.   mem = %s B   path = '%s'\n",
+          engine->name, name, RKIntegerToCommaStyleString(mem), me->path);
+    
+    pthread_mutex_unlock(&engine->mutex);
+    
+    DIR *did = opendir(me->path);
+    if (did == NULL) {
+        RKLog("%s Error. Unable to access directory '%s'\n", engine->name, me->path);
+        return (void *)1;
+    }
     
     // List all the files. Keep a copy of the list
     k = 0;
@@ -75,7 +87,7 @@ static void *fileWatcher(void *in) {
     size_t totalUsage = 0;
     while ((dir = readdir(did)) != NULL) {
         if (dir->d_type == DT_REG) {
-            sprintf(filenames[k], "%s/%s", path, dir->d_name);
+            sprintf(filenames[k], "%s/%s", me->path, dir->d_name);
             stat(filenames[k], &fileStat);
             // Get creation time
             indexedStats[k].index = k;
@@ -101,7 +113,70 @@ static void *fileWatcher(void *in) {
             usleep(100000);
         }
     }
+    
+    free(filenames);
+    free(indexedStats);
+    
+    RKLog(">%s %s Stopped.\n", engine->name, name);
+    
+    return NULL;
+}
 
+static void *folderWatcher(void *in) {
+    RKFileManager *engine = (RKFileManager *)in;
+    
+    int k;
+    
+    RKLog("%s Started.   mem = %s B  state = %x\n", engine->name, RKIntegerToCommaStyleString(engine->memoryUsage), engine->state);
+    
+    engine->state |= RKEngineStateActive;
+    engine->state ^= RKEngineStateActivating;
+    
+    char folders[][RKNameLength] = {"iq", "moment", "health"};
+    
+    engine->workerCount = sizeof(folders) / RKNameLength;
+    
+    engine->workers = (RKFileRemover *)malloc(engine->workerCount * sizeof(RKFileRemover));
+    if (engine->workers == NULL) {
+        RKLog(">%s Error. Unable to allocate an RKFileRemover.\n", engine->name);
+        return (void *)RKResultFailedToCreateFileRemover;
+    }
+    memset(engine->workers, 0, engine->workerCount * sizeof(RKFileRemover));
+    
+    for (k = 0; k < engine->workerCount; k++) {
+        RKFileRemover *worker = &engine->workers[k];
+        
+        worker->id = k;
+        worker->parent = engine;
+
+        // Raw data path
+        if (engine->radarDescription != NULL && strlen(engine->radarDescription->dataPath)) {
+            snprintf(worker->path, RKMaximumPathLength - 1, "%s/%s", engine->radarDescription->dataPath, folders[k]);
+        } else if (strlen(engine->dataPath)) {
+            snprintf(worker->path, RKMaximumPathLength - 1, "%s/%s", engine->dataPath, folders[k]);
+        } else {
+            snprintf(worker->path, RKMaximumPathLength - 1, "%s", folders[k]);
+        }
+        printf("%d. %s\n", k, worker->path);
+        
+        if (pthread_create(&worker->tid, NULL, fileRemover, worker) != 0) {
+            RKLog(">%s Error. Failed to start a file remover.\n", engine->name);
+            return (void *)RKResultFailedToStartFileRemover;
+        }
+    }
+
+    while (engine->state & RKEngineStateActive) {
+        k = 0;
+        while (k++ < 10 && engine->state & RKEngineStateActive) {
+            usleep(100000);
+        }
+    }
+
+    for (k = 0; k < engine->workerCount; k++) {
+        pthread_join(engine->workers[k].tid, NULL);
+    }
+    free(engine->workers);
+    
     return NULL;
 }
 
@@ -118,6 +193,7 @@ RKFileManager *RKFileManagerInit() {
             rkGlobalParameters.showColor ? RKGetBackgroundColorOfIndex(2) : "", rkGlobalParameters.showColor ? RKNoColor : "");
     engine->state = RKEngineStateAllocated;
     engine->memoryUsage = sizeof(RKFileManager);
+    pthread_mutex_init(&engine->mutex, NULL);
     return engine;
 }
 
@@ -125,6 +201,7 @@ void RKFileManagerFree(RKFileManager *engine) {
     if (engine->state & RKEngineStateActive) {
         RKFileManagerStop(engine);
     }
+    pthread_mutex_destroy(&engine->mutex);
     free(engine);
 }
 
@@ -155,7 +232,7 @@ int RKFileManagerStart(RKFileManager *engine) {
         RKLog("%s Starting ...\n", engine->name);
     }
     engine->state |= RKEngineStateActivating;
-    if (pthread_create(&engine->tidFileWatcher, NULL, fileWatcher, engine) != 0) {
+    if (pthread_create(&engine->tidFileWatcher, NULL, folderWatcher, engine) != 0) {
         RKLog("Error. Failed to start a ray gatherer.\n");
         return RKResultFailedToStartFileManager;
     }
