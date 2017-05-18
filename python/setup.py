6bbff4826cf4c0ecbe32b338d5c679c287675a79
boonleng <boonleng@ou.edu>
2017-05-18 11:33:51 -0500

    It compiles!

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup, Extension
 
 # Define the extension module
-radarkit = Extension('radarkit', sources=['radarkit.c'])
+radarkit = Extension('radarkit', sources=['radarkitmodule.c'])
 
 # Run the setup
 setup(ext_modules=[radarkit])
