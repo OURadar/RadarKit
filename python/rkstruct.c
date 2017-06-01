0cf84e7484cb7299b2f5be31a70dd7a3f121655c
boonleng <boonleng@ou.edu>
2017-06-01 09:29:03 -0500

    Two algorithms in a folder for collective processing

--- python/rkstruct.c
+++ python/rkstruct.c
@@ -1,33 +1,43 @@
 #include <Python.h>
 #include <RadarKit.h>
 
-// Wrapper
-static PyObject *rkstruct_test(PyObject *self, PyObject *args, PyObject *keywords) {
-    printf("rkstruct_test()\n");
-
+// Wrappers
+static PyObject *PyRKInit(PyObject *self, PyObject *args, PyObject *keywords) {
     RKSetWantScreenOutput(true);
-    RKShowTypeSizes();
+    Py_INCREF(Py_None);
+    return Py_None;
+}
 
+static PyObject *PyRKTest(PyObject *self, PyObject *args, PyObject *keywords) {
+    RKShowTypeSizes();
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject *PyRKTestShowColors(PyObject *self, PyObject *args, PyObject *keywords) {
+    RKTestShowColors();
     Py_INCREF(Py_None);
     return Py_None;
 }
 
 // Standard boiler plates
-static PyMethodDef rkstructMethods[] = {
-	{"test", (PyCFunction)rkstruct_test, METH_VARARGS | METH_KEYWORDS, "Test module"},
+static PyMethodDef PyRKMethods[] = {
+    {"init",       (PyCFunction)PyRKInit,           METH_VARARGS | METH_KEYWORDS, "Init module"},
+	{"test",       (PyCFunction)PyRKTest,           METH_VARARGS | METH_KEYWORDS, "Test module"},
+    {"showColors", (PyCFunction)PyRKTestShowColors, METH_VARARGS | METH_KEYWORDS, "Color module"},
 	{NULL, NULL, 0, NULL}
 };
 
-static struct PyModuleDef rkstructmodule = {
+static struct PyModuleDef PyRKModule = {
     PyModuleDef_HEAD_INIT,
-    "rkstruct",
+    "rk",
     NULL,
     -1,
-    rkstructMethods
+    PyRKMethods
 };
 
 PyMODINIT_FUNC
 
 PyInit_rkstruct(void) {
-    return PyModule_Create(&rkstructmodule);
+    return PyModule_Create(&PyRKModule);
 }
