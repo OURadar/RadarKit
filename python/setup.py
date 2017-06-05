93ac75afafe411da2a3295c70457f75fd6835556
boonleng <boonleng@ou.edu>
2017-06-05 09:09:10 -0500

    Python module into another project

--- python/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup, Extension
-
-# Define the extension module
-rkstruct = Extension('rkstruct', sources=['rkstruct.c'])
-
-# Run the setup
-setup(ext_modules=[rkstruct])
