f1cb09c5286f65ef34ba5f124fd5a9b795d987ac
boonleng <boonleng@ou.edu>
2017-06-27 13:24:17 -0500

    MATLAB reader updated

--- matlab/rkr.m
+++ matlab/rkr.m
@@ -36,45 +36,45 @@ xlim([0 100])
 figure(2)
 clf
 M = 22;
-ha = zeros(M * N, 1);
-hd = zeros(M * N, 3);
-ht = zeros(M * N, 1);
+FIG.ax = zeros(M * N, 1);
+FIG.pl = zeros(M * N, 3);
+FIG.ht = zeros(M * N, 1);
 for ii = 1 : M * N
     k = ii;
     ix = rem(ii - 1, M);
     iy = N - 1 - floor((ii - 1) / M);
-    ha(ii) = axes('Unit', 'Normalized', 'Position', [0.02 + ix / M * 0.97, 0.05 + iy / N * 0.94, 0.92 / M, 0.88 / N]);
-    hd(ii, :) = plot(...
+    FIG.ax(ii) = axes('Unit', 'Normalized', 'Position', [0.02 + ix / M * 0.97, 0.05 + iy / N * 0.94, 0.92 / M, 0.88 / N]);
+    FIG.pl(ii, :) = plot(...
         t_fast, real(pulses(1:ng, k, 2)), ...
         t_fast, imag(pulses(1:ng, k, 2)), ...
         t_fast, abs(pulses(1:ng, k, 2)));
     grid on
     if iy == 0, xlabel('Time (us)'); else, set(gca, 'XTickLabel', []); end
     if ix == 0, ylabel('Amplitude'); else, set(gca, 'YTickLabel', []); end
-    ht(ii) = text(0.9 * t_fast(end), -10000, sprintf('%d / %d / %d', dat.pulses(k).n, dat.pulses(k).i, floor(rem(int32(dat.pulses(k).i), M) / 2) - 5));
+    FIG.ht(ii) = text(0.9 * t_fast(end), -10000, sprintf('%d / %d / %d', dat.pulses(k).n, dat.pulses(k).i, floor(rem(int32(dat.pulses(k).i), M) / 2) - 5));
     ylim(11500 * [-1 1])
 end
 boonlib('bsizewin', gcf, [3200 1080])
 set(gcf, 'Menubar', 'None');
-set(ht, 'HorizontalAlignment', 'Right');
-lp = linkprop(ha, {'XLim'});
+set(FIG.ht, 'HorizontalAlignment', 'Right');
+lp = linkprop(FIG.ax, {'XLim'});
 
 return
 
 %%
-%jj = 1;
-%while (jj < size(pulses, 2))
+jj = 1;
+while (jj < size(pulses, 2))
     for ii = 1 : M * N
         k = ii + jj;
-        if k > size(pulses, 2), break; end;
-        set(hd(ii, 1), 'YData', real(pulses(1:ng, k, 2)));
-        set(hd(ii, 2), 'YData', imag(pulses(1:ng, k, 2)));
-        set(hd(ii, 3), 'YData', abs(pulses(1:ng, k, 2)));
-        set(ht(ii), 'String', sprintf('%d (%d) / %d (%d)', ...
+        if k > size(pulses, 2), break; end
+        set(FIG.pl(ii, 1), 'YData', real(pulses(1:ng, k, 2)));
+        set(FIG.pl(ii, 2), 'YData', imag(pulses(1:ng, k, 2)));
+        set(FIG.pl(ii, 3), 'YData', abs(pulses(1:ng, k, 2)));
+        set(FIG.pl(ii), 'String', sprintf('%d (%d) / %d (%d)', ...
             dat.pulses(k).n, floor(rem(double(dat.pulses(k).n), M) / 2) - 5, ...
             dat.pulses(k).i, floor(rem(double(dat.pulses(k).i), M) / 2) - 5));
     end
     drawnow
     % bfig png
     jj = jj + M * N;
-%end
+end
