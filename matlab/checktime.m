0ff3255995d89a349a7fb96edfa7de48425ebf61
boonleng <boonleng@ou.edu>
2017-06-28 10:03:41 -0500

    Added maxPulse to reader, new script checktime

---
+++ matlab/checktime.m
@@ -0,0 +1,43 @@
+if ~exist('dat', 'var')
+    filename = boonlib('choosefile', '~/Downloads', '*.rkr');
+
+    dat = iqread(filename, 200);
+
+    fprintf('Rearranging data ...\n');
+
+    %%
+    disp(dat.pulses(1))
+
+    % Original data in I/Q, gate, channel, pulse count
+    pulses = cat(4, dat.pulses(:).iq);
+
+    % Marry I and Q into a complex number
+    pulses = complex(pulses(1, :, :, :), pulses(2, :, :, :));
+
+    % Reorder the indices to gate, pulse count, channel
+    pulses = permute(pulses, [2 4 3 1]);
+
+    pulses = single(pulses);
+end
+
+td = cat(1, dat.pulses(:).timeDouble);
+tv_sec = cat(1, dat.pulses(:).time_tv_sec);
+tv_usec = cat(1, dat.pulses(:).time_tv_usec);
+tv = double(tv_sec) + 1.0e-6 * double(tv_usec);
+
+
+%% 
+n = numel(td) - 1;
+figure(1003)
+clf
+FIG.lines = plot( 1:n, 1e3 * diff(tv), '.', 1:n, 1e3 * diff(td), '.');
+grid on
+legend('Raw', 'RadarKit')
+xlabel('Sample Index')
+ylabel('Time (usec)')
+title('Time Difference Between Samples')
+FIG.prop = linkprop(FIG.lines, {'XLim', 'YLim'});
+set(FIG.lines, 'MarkerSize', 18)
+xlim([50 100])
+pan on
+
