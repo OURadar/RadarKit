4ab6cc29d1011f560395e1b184b13c5c4f70cb6c
boonleng <boonleng@ou.edu>
2017-09-06 17:39:45 -0500

    Compilation tweaks for old Debian OS

--- headers/RadarKit/RKSIMD.h
+++ headers/RadarKit/RKSIMD.h
@@ -14,6 +14,13 @@
 #if defined(_MSC_VER)
 // Microsoft C/C++-compatible compiler
 #include <intrin.h>
+#elif defined(_EXPLICIT_INTRINSIC)
+// Old OSs that do not have x86intrin.h
+#include <pmmintrin.h>
+#include <mmintrin.h>
+#include <xmmintrin.h>
+#include <emmintrin.h>
+#include <smmintrin.h>
 #elif defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
 // GCC-compatible compiler, targeting x86/x86-64
 #include <x86intrin.h>
