36cf96d8a11fe476e3ef7538781c3fd939daf5b0
boonleng <boonleng@ou.edu>
2017-11-07 20:14:48 -0600

    This should work

--- RadarKitTest/main.c
+++ RadarKitTest/main.c
@@ -502,13 +502,14 @@ int main(int argc, const char **argv) {
         myRadar->desc.heading = 180.0f;
 
         RKWaveform *waveform = NULL;
-        const char wavfile[] = "waveforms/ofm.rkwav";
+        const char wavfile[] = "waveforms/ofmd.rkwav";
 //        const char wavfile[] = "/Users/boonleng/Developer/radarkit/waveforms/ofm.rkwav";
         if (RKFilenameExists(wavfile)) {
             RKLog("Loading waveform from file '%s'...\n", wavfile);
             waveform = RKWaveformInitFromFile(wavfile);
             RKWaveformSummary(waveform);
             RKLog("Adjusting waveform for RX sampling rate ...\n");
+            RKWaveformDownConvert(waveform, 2.0 * M_PI * 50.0 / 160.0);
             RKWaveformDecimate(waveform, 32);
             RKWaveformSummary(waveform);
         } else {
