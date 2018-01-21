9cd870a4ecf5e0c3a7a4aadfa2a61c57142b84c8
boonleng <boonleng@ou.edu>
2018-01-20 22:11:07 -0600

    Added more health keys

--- source/RKHealthLogger.c
+++ source/RKHealthLogger.c
@@ -230,6 +230,8 @@ static void *healthLogger(void *in) {
         } else {
             // Concatenate with latitude, longitude and heading values if GPS values are not reported
             sprintf(health->string + strlen(health->string) - 1, ", "
+                    "\"GPS Valid\":{\"Value\":true,\"Enum\":0}, "
+                    "\"GPS from File\":{\"Value\":true,\"Enum\":0}, "
                     "\"GPS Latitude\":{\"Value\":\"%.7f\",\"Enum\":0}, "
                     "\"GPS Longitude\":{\"Value\":\"%.7f\",\"Enum\":0}, "
                     "\"GPS Heading\":{\"Value\":\"%.2f\",\"Enum\":0}, "
