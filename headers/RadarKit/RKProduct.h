f8d6a8e77e608ae5c0b7a47cdbdff7238c9405a3
boonleng <boonleng@ou.edu>
2018-06-23 09:46:43 -0500

    Added RKProduct scaffold.

---
+++ headers/RadarKit/RKProduct.h
@@ -0,0 +1,14 @@
+//
+//  RKProduct.h
+//  RadarKit
+//
+//  Created by Boon Leng Cheong on 6/23/18.
+//  Copyright © Boon Leng Cheong. All rights reserved.
+//
+
+#ifndef __RadarKit_Product__
+#define __RadarKit_Product__
+
+#include <RadarKit/RKFoundation.h>
+
+#endif
---
+++ source/RKProduct.c
@@ -0,0 +1,19 @@
+//
+//  RKProduct.c
+//  RadarKit
+//
+//  Created by Boon Leng Cheong on 6/23/18.
+//  Copyright © Boon Leng Cheong. All rights reserved.
+//
+
+#include <RadarKit/RKProduct.h>
+
+#pragma mark - Helper Functions
+
+#pragma mark - Delegate Workers
+
+#pragma mark - Life Cycle
+
+#pragma mark - Properties
+
+#pragma mark - Interactions
