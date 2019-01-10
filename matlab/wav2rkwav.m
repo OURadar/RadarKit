7fafcaabceb28e28d4baf6f7573307f3dac34115
boonleng <boonleng@ou.edu>
2019-01-10 10:21:29 -0600

    Waveform writer 0.9

--- matlab/wav2rkwav.m
+++ matlab/wav2rkwav.m
@@ -5,7 +5,9 @@ m = 0.01;
 n = pw * fs;
 
 % Waveform samples
-wav = [cos(2 * pi * m * (0:n-1)).' sin(2 * pi * m * (0:n-1)).'];
+wav = exp(1i * 2 * pi * m * (0:n-1));
+wav = single(wav);
+wav = [real(wav); imag(wav)];
 samples = int16(65000 * wav);
 
 filename = 'xx.rkwav';
@@ -46,6 +48,31 @@ for g = 1:1
     b = b + 1 * fwrite(fid, zeros(1, 32 - b, 'uint8'), 'uint8');
     fprintf('Group header %d with %d bytes\n', g, b);
     % fwrite(waveform->filterAnchors[k], sizeof(RKFilterAnchor), groupHeader.filterCounts, fid);
-    % b = fwrite(b, 
+    name = 0;
+    origin = 0;
+    length = n;
+    inputOrigin = 0;
+    outputOrigin = 0;
+    maxDataLength = 2 ^ 18;
+    subCarrierFrequency = 0.0;
+    sensitivityGain = pw;
+    filterGain = 1.0;
+    fullScale = 65000;
+    b = 4 * fwrite(fid, 0, 'uint32');
+    b = b + 4 * fwrite(fid, origin, 'uint32');
+    b = b + 4 * fwrite(fid, length, 'uint32');
+    b = b + 4 * fwrite(fid, inputOrigin, 'uint32');
+    b = b + 4 * fwrite(fid, outputOrigin, 'uint32');
+    b = b + 4 * fwrite(fid, maxDataLength, 'uint32');
+    b = b + 4 * fwrite(fid, subCarrierFrequency, 'float');
+    b = b + 4 * fwrite(fid, sensitivityGain, 'float');
+    b = b + 4 * fwrite(fid, filterGain, 'float');
+    b = b + 4 * fwrite(fid, fullScale, 'float');
+    b = b + 1 * fwrite(fid, zeros(1, 64 - b, 'uint8'), 'uint8');
+    fprintf('Filter anchor %d with %d bytes\n', g, b);
+    % fwrite(waveform->samples[k], sizeof(RKComplex), groupHeader.depth, fid);
+    % fwrite(waveform->iSamples[k], sizeof(RKInt16C), groupHeader.depth, fid);
+    fwrite(fid, wav, 'float');
+    fwrite(fid, samples, 'float');
 end
 fclose(fid);
