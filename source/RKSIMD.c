OURadar/RadarKit

commit 21fbc6818f1a5891a39a7f9ccb9cb0982c9aec93

Small venture into SIMD stuff

source/RKSIMD.c
@@ -379,10 +379,12 @@ void RKSIMD_iymul(RKComplex *src, RKComplex *dst, const int n) {
 
 void RKSIMD_iymulc(RKComplex *src, RKComplex *dst, const int n) {
 	int k, K = (n * sizeof(RKComplex) + sizeof(RKVec) - 1) / sizeof(RKVec);
+    const float q[4] = {1.0, -1.0, 1.0, -1.0};
 	RKVec r, i, x;
 	RKVec *s = (RKVec *)src;                                     // [  a   b   x   y ]
 	RKVec *d = (RKVec *)dst;                                     // [  c   d   z   w ]
-	RKVec c = _rk_mm_set_pf(-1.0, 1.0, -1.0, 1.0);               // [  1  -1   1  -1 ]
+	//RKVec c = _rk_mm_set_pf(-1.0, 1.0, -1.0, 1.0);               // [  1  -1   1  -1 ]
+    RKVec c = _rk_mm_set_pf(q);                                  // [  1  -1   1  -1 ]
 	for (k = 0; k < K; k++) {
         *d = _rk_mm_mul_pf(*d, c);                               // [  c  -d   z  -w ]
 		r = _rk_mm_moveldup_pf(*s);                              // [  a   a   x   x ]

Makefile
@@ -16,9 +16,13 @@ endif
 
 CFLAGS += -std=gnu99
 CFLAGS += -Woverlength-strings
-CFLAGS += -march=native
-CFLAGS += -mfpmath=sse
 CFLAGS += -Wall -Wno-unknown-pragmas
+
+ifeq ($(MACHINE), x86_64)
+	CFLAGS += -march=native
+	CFLAGS += -mfpmath=sse
+endif
+
 CFLAGS += -I headers -I headers/RadarKit -fPIC
 
 LDFLAGS = -L./

RadarKit.xcodeproj/project.pbxproj
@@ -382,11 +382,11 @@
 				B565B53F1E5A3E23000C8503 /* RKWaveform.h */,
 				B5324DDA1AB8D9F100C00517 /* RKClient.h */,
 				B5324DDE1AB8D9F100C00517 /* RKServer.h */,
+				B549D3712646F05900EA6E51 /* RKMoment.h */,
 				B5E6D94B1E1C4A1200DFBBA9 /* RKPositionEngine.h */,
 				B5E6D9501E1C617A00DFBBA9 /* RKPedestalPedzy.h */,
 				B5C1F3501AB9C917004F88CD /* RKPulseEngine.h */,
 				B508EA751FB7AAB900A6A83C /* RKPulseRingFilter.h */,
-				B549D3712646F05900EA6E51 /* RKMoment.h */,
 				B5B5196B1DCA87BD00470924 /* RKPulsePair.h */,
 				B5DDE7E31E1AB57700C5D16D /* RKMultiLag.h */,
 				B548350F216B9E280066FA0D /* RKSpectralMoment.h */,
@@ -459,11 +459,11 @@
 				B5FFD4611FB0B5C200319453 /* RKWaveform.c */,
 				B5FFD4621FB0B5C300319453 /* RKClient.c */,
 				B5FFD4651FB0B5C300319453 /* RKServer.c */,
+				B549D3732646F07800EA6E51 /* RKMoment.c */,
 				B5FFD4741FB0B5C500319453 /* RKPositionEngine.c */,
 				B5FFD4781FB0B5C600319453 /* RKPedestalPedzy.c */,
 				B5FFD4691FB0B5C400319453 /* RKPulseEngine.c */,
 				B508EA771FB7AAD500A6A83C /* RKPulseRingFilter.c */,
-				B549D3732646F07800EA6E51 /* RKMoment.c */,
 				B5FFD4671FB0B5C300319453 /* RKPulsePair.c */,
 				B5FFD45F1FB0B5C200319453 /* RKMultiLag.c */,
 				B5483511216B9E8F0066FA0D /* RKSpectralMoment.c */,

headers/RadarKit/RKSIMD.h
@@ -48,9 +48,9 @@ typedef __m256i RKVecCvt;
 #define _rk_mm_sub_pf(a, b)          _mm512_sub_ps(a, b)
 #define _rk_mm_mul_pf(a, b)          _mm512_mul_ps(a, b)
 #define _rk_mm_div_pf(a, b)          _mm512_div_ps(a, b)
-#define _rk_mm_addsub_pf(a, b)       _mm512_addsub_ps(a, b)
 #define _rk_mm_set1_pf(a)            _mm512_set1_ps(a)
-#define _rk_mm_set_pf(a, b, c, d)    _mm512_set_ps(a, b, c, d, a, b, c, d, a, b, c, d, a, b, c, d)
+//#define _rk_mm_set_pf(a, b, c, d)    _mm512_set_ps(a, b, c, d, a, b, c, d, a, b, c, d, a, b, c, d)
+#define _rk_mm_set_pf(v)             _mm512_set_ps(v[0], v[1], v[2], v[3], v[0], v[1], v[2], v[3], v[0], v[1], v[2], v[3], v[0], v[1], v[2], v[3])
 #define _rk_mm_movehdup_pf(a)        _mm512_movehdup_ps(a)
 #define _rk_mm_moveldup_pf(a)        _mm512_moveldup_ps(a)
 #define _rk_mm_shuffle_pf(a, b, m)   _mm512_shuffle_ps(a, b, m)
@@ -74,9 +74,9 @@ typedef __m128i RKVecCvt;
 #define _rk_mm_sub_pf(a, b)          _mm256_sub_ps(a, b)
 #define _rk_mm_mul_pf(a, b)          _mm256_mul_ps(a, b)
 #define _rk_mm_div_pf(a, b)          _mm256_div_ps(a, b)
-#define _rk_mm_addsub_pf(a, b)       _mm256_addsub_ps(a, b)
 #define _rk_mm_set1_pf(a)            _mm256_set1_ps(a)
-#define _rk_mm_set_pf(a, b, c, d)    _mm256_set_ps(a, b, c, d, a, b, c, d)
+//#define _rk_mm_set_pf(a, b, c, d)    _mm256_set_ps(a, b, c, d, a, b, c, d)
+#define _rk_mm_set_pf(v)    _mm256_set_ps(v[0], v[1], v[2], v[3], v[0], v[1], v[2], v[3])
 #define _rk_mm_movehdup_pf(a)        _mm256_movehdup_ps(a)
 #define _rk_mm_moveldup_pf(a)        _mm256_moveldup_ps(a)
 #define _rk_mm_shuffle_pf(a, b, m)   _mm256_shuffle_ps(a, b, m)
@@ -96,16 +96,16 @@ typedef __m128i RKVecCvt;
 //#define _rk_mm_log10_pf(a)           _mm256_log10_ps(a)
 //#endif
 
-#else
+#elif defined(_EXPLICIT_INTRINSIC)
 
 typedef __m128 RKVec;
 #define _rk_mm_add_pf(a, b)          _mm_add_ps(a, b)
 #define _rk_mm_sub_pf(a, b)          _mm_sub_ps(a, b)
 #define _rk_mm_mul_pf(a, b)          _mm_mul_ps(a, b)
 #define _rk_mm_div_pf(a, b)          _mm_div_ps(a, b)
-#define _rk_mm_addsub_pf(a, b)       _mm_addsub_ps(a, b)
 #define _rk_mm_set1_pf(a)            _mm_set1_ps(a)
-#define _rk_mm_set_pf(a, b, c, d)    _mm_set_ps(a, b, c, d)
+//#define _rk_mm_set_pf(a, b, c, d)    _mm_set_ps(a, b, c, d)
+#define _rk_mm_set_pf(v)             _mm_set_ps(v[0], v[1], v[2], v[3])
 #define _rk_mm_movehdup_pf(a)        _mm_movehdup_ps(a)
 #define _rk_mm_moveldup_pf(a)        _mm_moveldup_ps(a)
 #define _rk_mm_shuffle_pf(a, b, m)   _mm_shuffle_ps(a, b, m)
@@ -126,6 +126,29 @@ typedef __m128 RKVec;
 //#if defined(_mm_mul_ps)
 //#define _rk_mm_log10_pf(a)           _mm_log10_ps(a)
 //#endif
+
+#elif defined(__ARM_NEON__)
+
+typedef float32x4_t RKVec;
+#define _rk_mm_add_pf(a, b)          vaddq_f32(a, b)
+#define _rk_mm_sub_pf(a, b)          vsubq_f32(a, b)
+#define _rk_mm_mul_pf(a, b)          vmulq_f32(a, b)
+#define _rk_mm_div_pf(a, b)          vmulq_f32(a, vrecpeq_f32(b))
+#define _rk_mm_set1_pf(a)            vld1q_dup_f32((float *)&a)
+#define _rk_mm_set_pf(v)             vld1q_f32(v)
+#define _rk_mm_movehdup_pf(a)        _mm_movehdup_ps(a)
+#define _rk_mm_moveldup_pf(a)        _mm_moveldup_ps(a)
+#define _rk_mm_shuffle_pf(a, b, m)   _mm_shuffle_ps(a, b, m)
+#define _rk_mm_fmaddsub_pf(a, b, c)  _mm_addsub_ps(_mm_mul_ps(a, b), c)
+#define _rk_mm_setzero_si()          _mm_setzero_si128()
+#define _rk_mm_unpacklo_epi16(a, b)  _mm_unpacklo_epi16(a, b)
+#define _rk_mm_unpackhi_epi16(a, b)  _mm_unpackhi_epi16(a, b)
+#define _rk_mm_cvtepi16_epi32(a)     _mm_cvtepi16_epi32(a)
+#define _rk_mm_cvtepi32_pf(a)        _mm_cvtepi32_ps(a)
+#define _rk_mm_sqrt_pf(a)            vsqrtq_f32(a)
+#define _rk_mm_rcp_pf(a)             _mm_rcp_ps(a)
+#define _rk_mm_max_pf(a, b)          _mm_max_ps(a, b)
+#define _rk_mm_min_pf(a, b)          _mm_min_ps(a, b)
 
 #endif
 

commit 335fd80327ef345bcce1faf960e7c9616a00a827

Fixed a bug introduced in 21fbc681

source/RKSIMD.c
@@ -379,7 +379,7 @@ void RKSIMD_iymul(RKComplex *src, RKComplex *dst, const int n) {
 
 void RKSIMD_iymulc(RKComplex *src, RKComplex *dst, const int n) {
 	int k, K = (n * sizeof(RKComplex) + sizeof(RKVec) - 1) / sizeof(RKVec);
-    const float q[4] = {1.0, -1.0, 1.0, -1.0};
+    const float q[4] = {-1.0, 1.0, -1.0, 1.0};
 	RKVec r, i, x;
 	RKVec *s = (RKVec *)src;                                     // [  a   b   x   y ]
 	RKVec *d = (RKVec *)dst;                                     // [  c   d   z   w ]
@@ -503,7 +503,7 @@ void RKSIMD_Int2Complex(RKInt16C *src, RKComplex *dst, const int n) {
     }
 
 #else
-    
+
     RKInt16C *s = src;
     RKComplex *d = dst;
     for (k = 0; k < n; k++) {